e0ef4fb609494c65a54901527767ee37abb12bf7
markz0r <root@ip-172-20-38-80.ap-southeast-2.compute.internal>
2019-09-24 03:38:45 +0000

    cron fixes

--- cron/eramba_yearly.sh
+++ cron/eramba_yearly.sh
@@ -1,3 +1,3 @@
 docker exec -i eramba-community-docker_app_1 bash -c '/var/www/sites/eramba_community/app/Console/cake cron job yearly'
-chown -R apache:apache /var/www/sites/eramba_community
-chmod 777 -R /var/www/sites/eramba_community/app/tmp
+docker exec -i eramba-community-docker_app_1 bash -c 'chown -R apache:apache /var/www/sites/eramba_community'
+docker exec -i eramba-community-docker_app_1 bash -c 'chmod 777 -R /var/www/sites/eramba_community/app/tmp'
